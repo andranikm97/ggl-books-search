a5eab63668972b2ce729af462c8ff611f98c30ad
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-19 17:50:38 +0300

    Create: introduce Book structure
    A simplified structure of the Book component for testing purposes

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Book = (props) => {
-  console.log(props);
+  return (
+    <div className='book'>
+      <img className='book-cover' src='' alt='' />
+      <h3 className='book-category'></h3>
+      <h2 className='book-title'></h2>
+      <h2 className='book-author'></h2>
+    </div>
+  );
 };
 
 export default Book;
