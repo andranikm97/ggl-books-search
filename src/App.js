4b7cd816379c3efa994a52f439022236e938c042
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-19 17:47:13 +0300

    Feat&Style: make simple structure and apply style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import Search from './Search';
 
 function App() {
   const [loading, setFalse] = useState(false);
+  const [request, setRequest] = useState(false);
   const [books, setBooks] = useState([]);
 
-  return <Search />;
+  return (
+    <div className='main-container'>
+      <Search />
+      {request ? <h1> Search for some books! </h1> : 'books...'}
+    </div>
+  );
 }
 
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import './styles/search.css';
 
 const Search = () => {
-  const [state, setState] = useState({
+  const initialState = {
     query: '',
     category: '',
     sort: '',
-  });
+  };
+
+  const [state, setState] = useState(initialState);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -15,15 +18,42 @@ const Search = () => {
     }));
   };
 
+  const { query, category, sort } = state;
+
   return (
     <div className='search-container'>
-      <div>
+      <div className='search-field'>
         <input
-          value={state.query}
+          name='query'
+          className='search-input'
+          value={query}
           onChange={handleChange}
           type='text'
           placeholder='Enter book name, author, category or etc.'
         />
+        <button className='search-button'> Search </button>
+      </div>
+      <div className='search-options'>
+        <div>
+          <h3> Category </h3>
+          <select name='category' value={category} onChange={handleChange}>
+            <option value='all'>All categories</option>
+            <option value='art'>Art</option>
+            <option value='biography'>Biography</option>
+            <option value='computers'>Computers</option>
+            <option value='history'>History</option>
+            <option value='medical'>Medical</option>
+            <option value='poetry'>Poetry</option>
+          </select>
+        </div>
+
+        <div>
+          <h3> Sort by </h3>
+          <select name='sort' value={sort} onChange={handleChange}>
+            <option value='newest'>Newest</option>
+            <option value='relevance'>Relevance</option>
+          </select>
+        </div>
       </div>
     </div>
   );
