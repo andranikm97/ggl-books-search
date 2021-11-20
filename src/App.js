f63e2e29b31588bbc99794c595bc8fac23ea5cb3
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-20 04:12:02 +0300

    Fix: change state management structure
    Change the books state management to include total count

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import Search from './Search';
+import Books from './Books';
+import Loader from './Loader';
 import { getBooks } from './apiWorker';
 
 function App() {
-  const [loading, setFalse] = useState(false);
+  // const [loading, setFalse] = useState(false);
   const [request, setRequest] = useState(false);
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState({
+    results: [],
+    totalFound: 0,
+  });
+  // const [total, setTotal] = useState(0);
 
   const submitSearch = (options) => {
-    return getBooks(options).then((data) => console.log(data));
+    setRequest(true);
+    return getBooks(options)
+      .then((data) => {
+        console.log(data);
+        // const { items } = data;
+        // const { total } = data.totalItems;
+        // setTotal();
+        setBooks({ results: data.items, totalFound: data.totalItems });
+      })
+      .then(() => setRequest(false));
   };
 
   return (
     <div className='main-container'>
       <Search submitSearch={submitSearch} />
-      {request ? <h1> Search for some books! </h1> : 'books...'}
+      <div className='data-container'>
+        {books.results.length !== 0 ? (
+          <Books books={books.results} totalFound={books.totalFound} />
+        ) : request ? (
+          <Loader />
+        ) : (
+          'Search for some books!'
+        )}
+      </div>
+      {/* {request ? <h1> Searching for some books! </h1> : 'not searching'} */}
     </div>
   );
 }
