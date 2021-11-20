26db41575af1a4a260c9141c6e251e90d2473165
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-20 22:39:46 +0300

    Feat: clear page on new query

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const submitSearch = (options) => {
     setRequest(true);
+    booksStore.clearPage();
     return getBooks(options, 1)
       .then((data) => {
         console.log(data);
