1b2d0261944e2cf41f8332141e79860d566d0926
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-20 17:37:05 +0300

    Feat: add API function
    Added a capability to add more books instead of loading new ones

--- src/apiWorker.js
+++ src/apiWorker.js
@@ -1,7 +1,13 @@
 import { buildRequestString } from './apiRequest';
 
-export const getBooks = (options) => {
-  return fetch(buildRequestString(options))
+export const getBooks = (options, page) => {
+  return fetch(buildRequestString(options, page))
     .then((res) => res.json())
-    .catch((err) => console.log(err)); //eslint-disable-line no-console
+    .catch((err) => console.log(err));
+};
+
+export const loadMoreBooks = (query, page) => {
+  return fetch(query + `&startIndex=${(page - 1) * 30}`)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 };
