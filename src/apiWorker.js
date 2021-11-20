43fdee91b544fec865f48d499133150517ec8e54
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-20 03:38:32 +0300

    Refactor: change api worker code
    The code was unreadable, and I decided to go with a different implementation

--- src/apiWorker.js
+++ src/apiWorker.js
@@ -1,16 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const API_URI = 'https://www.googleapis.com/books/v1/volumes?';
+const API_URI = 'https://www.googleapis.com/books/v1/volumes';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const getBooks = (options) => {
-  const { query, sort } = options;
-  const searchString = `key=${API_KEY}&q=${query.split(' ').join('+')}&sortBy=${
-    sort.trim() === '' ? 'newest' : sort
-  }&maxResults=30`;
+  const { query, category, sort } = options;
+  let url = API_URI;
+  let queryParams = {
+    key: API_KEY,
+    q: query.split(' ').join('+'),
+    sortBy: sort.trim() === '' ? 'newest' : sort,
+    maxResults: 30,
+  };
 
-  return fetch(API_URI + searchString)
+  if (category !== 'all') {
+    queryParams.q += `+subject:${category}`;
+  }
+
+  queryParams = new URLSearchParams(queryParams);
+  return fetch(url + '?' + queryParams.toString())
     .then((res) => res.json())
     .catch((err) => console.log(err)); //eslint-disable-line no-console
 };
