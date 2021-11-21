b7caf36aeb7101c701a0f4e61042fe7352047ef7
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-21 21:08:53 +0300

    Fix: disappering results count
    The results count display was scrolling with the books list. Now it stays fixed on top.

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -4,14 +4,19 @@ import './books.css';
 
 const Books = ({ books, totalFound, searchForMore }) => {
   return (
-    <div className='books-container'>
-      <span> {`Found ${totalFound} results`}</span>
-      {!!books.length &&
-        books.map((book) => {
-          return <Book key={book._id} book={book} />;
-        })}
-      <div className='load-more-btn'>
-        <button onClick={searchForMore}> Load more </button>
+    <div className='results-container'>
+      <div className='results-count'>
+        {' '}
+        <span>{`Found ${totalFound} results`}</span>
+      </div>
+      <div className='books-container'>
+        {!!books.length &&
+          books.map((book) => {
+            return <Book key={book._id} book={book} />;
+          })}
+        <div className='load-more-btn'>
+          <button onClick={searchForMore}> Load more </button>
+        </div>
       </div>
     </div>
   );
