f0a8707c7d8846aee01d793d042b08b611a13179
Andranik Markaryan <andranik.mrk@gmail.com>
2021-11-24 01:15:51 +0300

    Chore: remove unnecessary props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { toJS } from 'mobx';
-import './styles/App.css';
-import Search from './components/Search/Search';
-import Books from './components/Books/Books';
-import Loader from './components/Loader/Loader';
-import BookDetail from './components/BookDetail/BookDetail';
-import ErrorContainer from './components/ErrorContainer/ErrorContainer';
-import { useBooksStore } from './contexts/BooksContext';
-import { Observer } from 'mobx-react-lite';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { toJS } from "mobx";
+import "./styles/App.css";
+import Search from "./components/Search/Search";
+import Books from "./components/Books/Books";
+import Loader from "./components/Loader/Loader";
+import BookDetail from "./components/BookDetail/BookDetail";
+import ErrorContainer from "./components/ErrorContainer/ErrorContainer";
+import { useBooksStore } from "./contexts/BooksContext";
+import { Observer } from "mobx-react-lite";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   const booksStore = useBooksStore();
@@ -16,20 +16,20 @@ const App = () => {
   return (
     <Observer>
       {() => (
-        <div className='main-container'>
+        <div className="main-container">
           <Router>
             <Search
-              submitSearch={booksStore.submitSearch}
-              page={toJS(booksStore.page)}
+            // submitSearch={booksStore.submitSearch}
+            // page={toJS(booksStore.page)}
             />
             <Switch>
-              <Route path='/details/:id'>
-                <div className='data-container'>
+              <Route path="/details/:id">
+                <div className="data-container">
                   <BookDetail />
                 </div>
               </Route>
-              <Route path='/'>
-                <div className='data-container'>
+              <Route path="/">
+                <div className="data-container">
                   {booksStore.waitingOnRequest ? (
                     <Loader />
                   ) : toJS(booksStore.books).length !== 0 ? (
@@ -44,8 +44,8 @@ const App = () => {
                       message={`Invalid search, please enter a valid query...`}
                     />
                   ) : (
-                    <p className='no-books-message '>
-                      Enter a query and hit <i className='fa fa-search' /> or
+                    <p className="no-books-message ">
+                      Enter a query and hit <i className="fa fa-search" /> or
                       'Enter'
                     </p>
                   )}
@@ -53,7 +53,7 @@ const App = () => {
               </Route>
             </Switch>
           </Router>
-          <footer className='footer'></footer>
+          <footer className="footer"></footer>
         </div>
       )}
     </Observer>
